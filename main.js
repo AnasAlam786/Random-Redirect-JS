c699b7a7e63566f99d27164a25832718e046ca9e
AnasAlam786 <74352709+AnasAlam786@users.noreply.github.com>
2024-07-07 15:00:47 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const number=Math.floor(Math.random()*3)
+if (number == 0){
+  
+//document.getElementById('frame').src = "https://sites.google.com/view/assets-and-liability/home"
+document.getElementById('frame').src = "https://master-template-one-click-hugo-cms.netlify.app/"
+//open("https://www.google.com/url?q=https%3A%2F%2Fwww.truebusiness.online%2F2023%2F01%2FWhat-is-Assets-and-Liability.html&sa=D&sntz=1&usg=AOvVaw0z6jx30h35aIRcxuS6L4xP","_blank","fullscreen=yes, focus=true")
+open("https://www.bing.com/ck/a?!&&p=062a83371b9fa99bJmltdHM9MTY4NTc1MDQwMCZpZ3VpZD0yY2FkZjlmYi0zYjI4LTY0NzUtMzVjNy1lYWVkM2E5YTY1MWMmaW5zaWQ9NTI1NA&ptn=3&hsh=3&fclid=2cadf9fb-3b28-6475-35c7-eaed3a9a651c&psq=Importance+of+Assets+and+Liabilities+True+Business&u=a1aHR0cHM6Ly93d3cudHJ1ZWJ1c2luZXNzLm9ubGluZS8yMDIzLzAxL1doYXQtaXMtQXNzZXRzLWFuZC1MaWFiaWxpdHkuaHRtbA&ntb=1","_blank","fullscreen=yes, focus=true")
+}
+  
+  
+else if (number == 1){
+  
+//document.getElementById('frame').src="https://sites.google.com/view/how-to-become-investor/home"
+document.getElementById('frame').src = "https://master-template-one-click-hugo-cms.netlify.app/"
+//open("https://www.google.com/url?q=https%3A%2F%2Fwww.truebusiness.online%2F2021%2F02%2FHow-To-Become-An-Independent-Invester-Tips.html&sa=D&sntz=1&usg=AOvVaw1GNEEEwLohm1IhTiOMyvcS","_blank","fullscreen=yes, focus=true")
+open("https://www.bing.com/ck/a?!&&p=fa9e94cc8b6e2a2cJmltdHM9MTY4NTc1MDQwMCZpZ3VpZD0yY2FkZjlmYi0zYjI4LTY0NzUtMzVjNy1lYWVkM2E5YTY1MWMmaW5zaWQ9NTI0NA&ptn=3&hsh=3&fclid=2cadf9fb-3b28-6475-35c7-eaed3a9a651c&psq=How+To+Become+An+Investor+True+Business&u=a1aHR0cHM6Ly93d3cudHJ1ZWJ1c2luZXNzLm9ubGluZS8yMDIxLzAyL0hvdy1Uby1CZWNvbWUtQW4tSW5kZXBlbmRlbnQtSW52ZXN0ZXItVGlwcy5odG1s&ntb=1","_blank","fullscreen=yes, focus=true")
+}
+  
+  
+else if(number==2){
+  
+//document.getElementById('frame').src ="https://sites.google.com/view/what-is-manufacturing-bussines/home"
+document.getElementById('frame').src = "https://master-template-one-click-hugo-cms.netlify.app/"
+//open("https://www.google.com/url?q=https%3A%2F%2Fwww.truebusiness.online%2F2021%2F01%2FHow-To-Start-Manufacturing-Business.html&sa=D&sntz=1&usg=AOvVaw2PORhm8xvPFvrT4GSVRatZ","_blank","fullscreen=yes, focus=true")
+open("https://www.bing.com/ck/a?!&&p=6e48d885928d5610JmltdHM9MTY4NTc1MDQwMCZpZ3VpZD0yY2FkZjlmYi0zYjI4LTY0NzUtMzVjNy1lYWVkM2E5YTY1MWMmaW5zaWQ9NTI4Ng&ptn=3&hsh=3&fclid=2cadf9fb-3b28-6475-35c7-eaed3a9a651c&psq=How+to+Start+A+Manufacturing+Business+True+Business&u=a1aHR0cHM6Ly93d3cudHJ1ZWJ1c2luZXNzLm9ubGluZS8yMDIxLzAxL0hvdy1Uby1TdGFydC1NYW51ZmFjdHVyaW5nLUJ1c2luZXNzLmh0bWw&ntb=1","_blank","fullscreen=yes, focus=true")
+}
+
